2bc12f7ea8278598ea936e84346c51b29e0c866c
thduck284 <0365946542az@gmail.com>
2026-02-01 12:51:47 +0700

    model index

---
+++ src/models/index.js
@@ -0,0 +1,40 @@
+// Auth Models
+export { User, RefreshToken } from './auth/index.js'
+
+// Learning Models
+export { 
+  Skill, 
+  Lesson, 
+  UserLessonProgress, 
+  UserSkillStats, 
+  UserDailyGoal 
+} from './learning/index.js'
+
+// Gamification Models
+export { 
+  Achievement, 
+  UserAchievement, 
+  Challenge, 
+  ChallengeParticipant, 
+  Game, 
+  GameSession, 
+  LeaderboardSnapshot 
+} from './gamification/index.js'
+
+// Social Models
+export { 
+  Friendship, 
+  Group, 
+  GroupMember, 
+  Post, 
+  PostLike, 
+  Comment 
+} from './social/index.js'
+
+// System Models
+export { 
+  Notification, 
+  ActivityLog, 
+  ChatbotConversation, 
+  ChatbotMessage 
+} from './system/index.js'
