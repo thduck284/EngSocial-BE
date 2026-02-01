9a77cc17a8eaf3915af2868b7b69b0fab968953d
thduck284 <0365946542az@gmail.com>
2026-02-01 12:52:27 +0700

    middleware index

---
+++ src/middlewares/index.js
@@ -0,0 +1,4 @@
+// Export all middlewares
+export { auth, optionalAuth } from './auth.middleware.js'
+export { errorHandler, notFound } from './error.middleware.js'
+export { validate } from './validate.middleware.js'
